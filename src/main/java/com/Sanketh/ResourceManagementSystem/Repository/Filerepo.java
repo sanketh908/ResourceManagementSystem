cce1ee88f429feb0d96ab2d525036e5d7b3090ae
sanketh908 <sankeths908@gmail.com>
2026-03-07 21:46:18 +0530

    new commit

---
+++ src/main/java/com/Sanketh/ResourceManagementSystem/Repository/Filerepo.java
@@ -0,0 +1,6 @@
+package com.Sanketh.ResourceManagementSystem.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Filerepo  extends JpaRepository<Filerepo, Integer> {
+}
