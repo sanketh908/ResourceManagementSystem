dab6d0a5b62e8218d55f31f55d1062e6d44a2a24
sanketh908 <sankeths908@gmail.com>
2026-03-08 19:59:30 +0530

    new commit

--- src/main/java/com/Sanketh/ResourceManagementSystem/Controller/FileController.java
+++ src/main/java/com/Sanketh/ResourceManagementSystem/Controller/FileController.java
@@ -2,10 +2,8 @@ package com.Sanketh.ResourceManagementSystem.Controller;
 
 import com.Sanketh.ResourceManagementSystem.Entity.Filemodul;
 import com.Sanketh.ResourceManagementSystem.Service.FileService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
@@ -20,6 +18,12 @@ public class FileController {
     @PostMapping("/add")
     public String addFile(@RequestParam("file") MultipartFile file) {
         fileService.addFile(file);
+
+    }
+    @
+    @GetMapping("")
+    public ResponseEntity<String> getFile("get/{id}") {
+
 
     }
 
