154e4b4b60aad253781f5454fca744da8411a597
sanketh908 <sankeths908@gmail.com>
2026-03-09 17:17:44 +0530

    new commit

--- src/main/java/com/Sanketh/ResourceManagementSystem/Security/UserDetailsService.java
+++ src/main/java/com/Sanketh/ResourceManagementSystem/Security/UserDetailsService.java
@@ -1,4 +1,24 @@
 package com.Sanketh.ResourceManagementSystem.Security;
 
-public class UserDetailsService {
+
+import com.Sanketh.ResourceManagementSystem.Entity.User;
+import com.Sanketh.ResourceManagementSystem.Repository.Userrepo;
+import org.springframework.security.core.parameters.P;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Optional;
+
+public class UserDetailsServic implements UserDetailsService {
+private final Userrepo userrepo;
+
+    public UserDetailsServic(Userrepo userrepo) {
+        this.userrepo = userrepo;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+      User user=userrepo.findByUsername(username).orElseThrow(()->new UsernameNotFoundException("User not found with username: "+username));
+    }
 }
