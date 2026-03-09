3c80b2c07eb29029cec65c583a562e2e13642eea
sanketh908 <sankeths908@gmail.com>
2026-03-09 17:33:57 +0530

    new commit

--- src/main/java/com/Sanketh/ResourceManagementSystem/Security/UserDetailsServiceImpl.java
+++ src/main/java/com/Sanketh/ResourceManagementSystem/Security/UserDetailsServiceImpl.java
@@ -1,24 +1,21 @@
 package com.Sanketh.ResourceManagementSystem.Security;
 
-
 import com.Sanketh.ResourceManagementSystem.Entity.User;
 import com.Sanketh.ResourceManagementSystem.Repository.Userrepo;
-import org.springframework.security.core.parameters.P;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-import java.util.Optional;
-
-public class UserDetailsServic implements UserDetailsService {
+public class UserDetailsServiceImpl implements UserDetailsService {
 private final Userrepo userrepo;
 
-    public UserDetailsServic(Userrepo userrepo) {
+    public UserDetailsServiceImpl(Userrepo userrepo) {
         this.userrepo = userrepo;
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-      User user=userrepo.findByUsername(username).orElseThrow(()->new UsernameNotFoundException("User not found with username: "+username));
+        User user =userrepo.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("Username not found"));
+        return new UserPrincipal(user);
     }
 }
---
+++ src/main/java/com/Sanketh/ResourceManagementSystem/Security/UserPrincipal.java
@@ -0,0 +1,37 @@
+package com.Sanketh.ResourceManagementSystem.Security;
+
+import com.Sanketh.ResourceManagementSystem.Entity.User;
+import org.jspecify.annotations.NullMarked;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+
+import org.springframework.security.core.userdetails.UserDetails;
+
+
+import java.util.Collection;
+import java.util.List;
+
+public class UserPrincipal implements UserDetails {
+private  final User user;
+    public UserPrincipal(User user) {
+        this.user=user;
+
+    }
+
+    @Override
+    @NullMarked
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+       return List.of(()->user.getRoles().name());
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    @NullMarked
+    public String getUsername() {
+        return user.getUsername();
+    }
+}
