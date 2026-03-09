351319bd385d8c196f3a4ac1b56a6a907dd1b9c5
sanketh908 <sankeths908@gmail.com>
2026-03-09 17:08:48 +0530

    new commit

--- src/main/java/com/Sanketh/ResourceManagementSystem/Repository/Userrepo.java
+++ src/main/java/com/Sanketh/ResourceManagementSystem/Repository/Userrepo.java
@@ -4,6 +4,9 @@ import com.Sanketh.ResourceManagementSystem.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface Userrepo extends JpaRepository<User,Integer> {
+    Optional<User> findByUsername(String username);
 }
---
+++ src/main/java/com/Sanketh/ResourceManagementSystem/Security/UserDetailsService.java
@@ -0,0 +1,4 @@
+package com.Sanketh.ResourceManagementSystem.Security;
+
+public class UserDetailsService {
+}
