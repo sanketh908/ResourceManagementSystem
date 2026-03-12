838392b7ebf52f0e50264eaee0264938e6015c75
sanketh908 <sankeths908@gmail.com>
2026-03-12 19:06:43 +0530

    new commit

--- src/main/java/com/Sanketh/ResourceManagementSystem/Security/SecurityConfig.java
+++ src/main/java/com/Sanketh/ResourceManagementSystem/Security/SecurityConfig.java
@@ -18,6 +18,7 @@ public class SecurityConfig {
             http
                     .csrf(AbstractHttpConfigurer::disable)
                     .authorizeHttpRequests(auth -> auth
+                            .requestMatchers("/home").permitAll()
                             .requestMatchers("/file/**").hasRole("ADMIN")
                             .requestMatchers("/userfiles/**").hasRole("USER")
                             .anyRequest().permitAll()
